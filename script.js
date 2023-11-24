e2e2bc2f886cfd9da17646a457dd1d52a32e84d3
Yuvraj Maurya <yuvraj95166@gmail.com>
2023-11-24 17:28:57 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 //your JS code here. If required.
+let text = document.getElementById("status");
+enterBtn.addEventListener("click",function(){
+	let heading = document.createElement("h1");
+	heading.innerText = "Entered Metaverse";
+	text.innerText="";
+	text.appendChild(heading);
+})
